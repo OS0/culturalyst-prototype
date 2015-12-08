ab8837071e00dfeec52eb8f87646ccae69a12cd1
rascle <ryan.bascle@gmail.com>
2015-12-07 18:32:16 -0600

    Added callback to getArtists
    Added a callback that sends an error if the database query fails, and sends the artists on success.

--- server/request-handler.js
+++ server/request-handler.js
@@ -13,5 +13,11 @@ var db = require('./db/schema.js')
 //Query all artists
 exports.getArtists = function(req, res) {
   //pull data from requests
-  db.getArtists();
+  db.getArtists(function(err, artists) {
+    if (err) {
+      res.status(401).send(err);
+    } else {
+      res.status(200).send(artists);
+    }
+  });
 }
