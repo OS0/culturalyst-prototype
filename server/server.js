ca025f08237d2efad036eb349e55773c368148c7
Brian Kustra <bkustrakb@gmail.com>
2015-12-09 12:06:56 -0600

    Change envPORT to env.PORT

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('client'));
-app.set('port', process.envPORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
