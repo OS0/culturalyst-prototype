89dc47eaaa3d1666c8d28aef54fa8726eb1a3e09
rascle <ryan.bascle@gmail.com>
2015-12-11 17:48:51 -0600

    Built out database schema for users.
    All users in one table (no separation).
    Mock Data button updates and shows you database objects.

--- server/db/schema.js
+++ server/db/schema.js
@@ -3,44 +3,70 @@ var sequelize = new Sequelize('culturalyst', 'root', null, {});
 //var Promise = require('bluebird');
 
 // Creatives
-var Creative = sequelize.define('creatives', {
-    name: Sequelize.STRING,
-    picurl: Sequelize.STRING,
-    medium: Sequelize.STRING,
-    submedium: Sequelize.STRING
+var User = sequelize.define('User', {
+  creative: Sequelize.BOOLEAN,
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  location: Sequelize.STRING,
+  bio: Sequelize.TEXT,
+  medium: Sequelize.STRING,
+  submedium: Sequelize.STRING,
+  picUrl: Sequelize.STRING
 });
 
+// var Content = sequelize.define('Content', {
+//   picUrl: Sequelize.STRING
+// })
+
+// //DATABASE RELATIONSHIPS
+// //users have many submedia, submedia has many users
+// User.belongsToMany(Media, { through: 'User_Media' });
+// Media.belongsToMany(User, { through: 'User_Media' });
+
+// //creative and catalyst join table
+// User.hasMany(User, { as: 'Catalysts' });
+
+// //users have many contents, contents have one user
+// User.hasMany(Content, { as: 'Content' });
 
 // Creates table if it does not exist
 sequelize.sync();
 
 
 // Helper functions
-exports.newCreative = function(name, picurl, medium, submedium, callback) {
-  Creative.findAll({ where: { name: name } })
-    .then(function(creatives){
-      if ( creatives.length > 0 ) {
-        callback('Name already taken')
+exports.newUser = function(creative, name, email, password, location, bio, medium, submedium, picUrl, callback) {
+  User.findAll({ where: { email: email } })
+    .then(function(users){
+      if ( users.length > 0 ) {
+        callback('Email already attached to account.')
       } else {
-        Creative.create({
+        User.create({
+          creative: creative,
           name: name,
-          picurl: picurl,
+          email: email,
+          password: password, //NEED TO BCRYPT!!!!!!!
+          location: location,
+          bio: bio,
           medium: medium,
-          submedium: submedium
+          submedium: submedium,
+          picUrl: picUrl
         })
         .catch(function(err) {
-          console.log('Create: ' + err);
+          if (err) {
+            console.log('While creating new user: ' + err);
+          }
         });
       }
     });
 };
 
 exports.getCreatives = function(callback) {
-  Creative.findAll().then(function(creatives){
+  User.findAll({ where: { creative: true } }).then(function(creatives){
     if (creatives.length) {
       callback(null, creatives);
     } else {
-      callback("We couldn't find any creatives")
+      callback("We couldn't find any creatives.");
     }
   });
 };
--- server/request-handler.js
+++ server/request-handler.js
@@ -3,16 +3,16 @@
 ///////////////////////
 
 //Database schema
-//var db = require('./db/schema.js');
+var db = require('./db/schema.js');
 
 ///////////////////////
 // Request-Handlers  //
 ///////////////////////
 
 //Create new Creative user
-exports.newCreative = function(req, res) {
-  var creative = req.body;
-  db.newCreative(creative.name, creative.picurl, creative.medium, creative.submedium, function(err, creatives) {
+exports.newUser = function(req, res) {
+  var user = req.body;
+  db.newUser(user.creative, user.name, user.email, user.password, user.location, user.bio, user.medium, user.submedium, user.picUrl, function(err, users) {
     if (err) {
       res.status(422).send(err);
     } else {
@@ -25,7 +25,8 @@ exports.newCreative = function(req, res) {
 exports.getCreatives = function(req, res) {
   //pull data from requests
   db.getCreatives(function(err, creatives) {
-    if (err) {
+    if (err && err !== "We couldn't find any creatives.") {
+      console.log(err);
       res.status(401).send(err);
     } else {
       res.status(200).send(creatives);
